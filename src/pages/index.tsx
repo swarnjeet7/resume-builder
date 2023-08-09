1e5786783eea3f5331e3231e1248efcebaa712c0
Swarnjeet Singh <swarnjeet7@gmail.com>
2023-08-09 23:21:15 +0530

    hooks added successfully

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
+import React, { type FC } from 'react';
 import Link from 'next/link';
 
-export default function Home() {
+export default function Home(): FC {
   return (
-    <>
-      <Link href="http://localhost:3000/create-cv/sections/index">
-            Create your resume
+    <Link href="http://localhost:3000/create-cv/sections/index">
+        Create your resume
       </Link>
-    </>
   );
 }
