f84375160b9fd25054a2ebe0b916a4c26888ba46
Swarnjeet Singh <swarnjeet7@gmail.com>
2023-08-09 23:23:52 +0530

    type fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import React, { type FC } from 'react';
+import React, { type ReactElement } from 'react';
 import Link from 'next/link';
 
-export default function Home(): FC {
+export default function Home(): ReactElement {
   return (
     <Link href="http://localhost:3000/create-cv/sections/index">
-        Create your resume
-      </Link>
+      Create your resume
+    </Link>
   );
 }
