2ad9a705b1731ad1ad15247fbf62f5bf991de70e
Swarnjeet Singh <swarnjeet7@gmail.com>
2023-08-13 00:35:25 +0530

    dummy change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,10 @@ import Link from 'next/link';
 export default function Home(): ReactElement {
   return (
     <div className="w-full">
-      <Link href="http://localhost:3000/create-cv/sections/index">
+      <Link
+        href="http://localhost:3000/create-cv/sections/index"
+        className="w-full"
+      >
         Create your resume
       </Link>
     </div>
